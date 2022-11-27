49f2434a4063d0e3f7353ab6597798af0c81ac0a
kodevasse <kodevasse@outlook.com>
2022-11-27 11:16:18 +0100

    first commit

---
+++ src/components/EventComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn" @click="$emit('danceIn')">{{ statuSE }}</button>
+  <button class="btn btn-error" @click="$emit('ranBowCheck')">
+    {{ rainBow }}
+  </button>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+const props = defineProps({
+  statuSE: {
+    type: String,
+    default: "DEFAULT",
+  },
+  eRR: {
+    type: String,
+  },
+  rainBow: {
+    type: String,
+    default: "no rainbow today",
+  },
+});
+// defineProps({
+//   statuSE: String,
+// });
+onMounted(() => {
+  console.log(props.eRR);
+});
+defineEmits(["danceIn", "ranBowCheck"]);
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { useLocalStorage, useMouse, usePreferredDark } from "@vueuse/core";
+import EventComponentVue from "../components/EventComponent.vue";
+import { ref, onMounted, computed } from "vue";
+// tracks mouse position
+const { x, y } = useMouse();
+
+// is user prefers dark theme
+const isDark = usePreferredDark();
+
+// persist state in localStorage
+const store = useLocalStorage("my-storage", {
+  name: "Apple",
+  color: "red",
+});
+const calcThis = () => {
+  console.log("EVENT FROM EventComponentVue");
+  state.value = !state.value;
+};
+const rainBowActivate = () => {
+  if (rainBow.value === "heavy") {
+    rainBow.value = "light";
+  } else {
+    rainBow.value = "heavy";
+  }
+};
+
+const state = ref(false);
+const e = computed(() => {
+  return state.value ? "fisk" : "duck";
+});
+
+const er = computed(() => {
+  if (e.value === "fisk") return "E IS FISK";
+  return "E IS NOT FISK";
+});
+const rainBow = ref("heavy");
+
+const erNOT = computed(() => {
+  if (e.value === "fisk") return "";
+  return "NOT";
+});
+</script>
+
+<template>
+  <main>
+    <EventComponentVue
+      :statuSE="e"
+      :eRR="er"
+      :rainBow="rainBow"
+      @dance-In="calcThis"
+      @ranBowCheck="rainBowActivate"
+    />
+    /// {{ e }} /// is
+    <span class="text-red-300">{{ erNOT }}</span>
+    fisk
+  </main>
+</template>
