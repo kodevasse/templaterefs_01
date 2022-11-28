a59b1aa169f6d3d2b5f43f1aeefb1f03b42e2658
kodevasse <kodevasse@outlook.com>
2022-11-28 10:44:22 +0100

    testing ListComponent

--- src/components/EventComponent.vue
+++ src/components/EventComponent.vue
@@ -1,12 +1,17 @@
 <template>
   <button class="btn" @click="$emit('danceIn')">{{ statuSE }}</button>
-  <button class="btn btn-error" @click="$emit('ranBowCheck')">
+  <button
+    class="p-2 w-16 rounded-full"
+    :class="rainBowColor"
+    @click="$emit('ranBowCheck')"
+  >
     {{ rainBow }}
   </button>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
+
 const props = defineProps({
   statuSE: {
     type: String,
@@ -23,8 +28,21 @@ const props = defineProps({
 // defineProps({
 //   statuSE: String,
 // });
+
+const rainBowColor = computed(() => {
+  if (props.rainBow === "heavy") return "bg-red-400 text-black";
+  else if (props.rainBow === "light")
+    return "bg-blue-300 text-black  bg-opacity-70";
+  else return "bg-gray-800";
+});
+
 onMounted(() => {
   console.log(props.eRR);
 });
 defineEmits(["danceIn", "ranBowCheck"]);
+
+// set timeout function
+setTimeout(() => {
+  console.log("Delayed for 1 second.");
+}, "1000");
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useLocalStorage, useMouse, usePreferredDark } from "@vueuse/core";
 import EventComponentVue from "../components/EventComponent.vue";
+import ListComponent from "@/components/ListComponent.vue";
 import { ref, onMounted, computed } from "vue";
 // tracks mouse position
 const { x, y } = useMouse();
@@ -40,19 +41,44 @@ const erNOT = computed(() => {
   if (e.value === "fisk") return "";
   return "NOT";
 });
+
+const item = {
+  name: "trond",
+  age: 32,
+  groups: ["fishing", "astronomy"],
+  address: { city: "Somewhere", country: "Norway" },
+};
+const items = [
+  {
+    name1: "LEIF",
+    age1: 12,
+    groups1: ["fishing", "astronomy"],
+    address1: { city: "Somewhere1", country: "Norway1" },
+  },
+  {
+    name1: "Veigar",
+    age1: 92,
+    groups1: ["fotball", "computer science"],
+    address1: { city: "Somewhere2", country: "Norway2" },
+  },
+];
 </script>
 
 <template>
   <main>
-    <EventComponentVue
-      :statuSE="e"
-      :eRR="er"
-      :rainBow="rainBow"
-      @dance-In="calcThis"
-      @ranBowCheck="rainBowActivate"
-    />
-    /// {{ e }} /// is
-    <span class="text-red-300">{{ erNOT }}</span>
-    fisk
+    <div class="flex flex-col w-full">
+      <EventComponentVue
+        :statuSE="e"
+        :eRR="er"
+        :rainBow="rainBow"
+        @dance-In="calcThis"
+        @ranBowCheck="rainBowActivate"
+      />
+      /// {{ e }} /// is
+      <span class="text-red-300">{{ erNOT }}</span>
+      fisk
+
+      <ListComponent :item="item" :items="items" />
+    </div>
   </main>
 </template>
